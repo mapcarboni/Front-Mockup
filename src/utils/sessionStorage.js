c57238ff79ffafdbd4fd8a4f619749f3c7d45593
mapcarboni <mapcarboni@gmail.com>
2025-05-14 14:46:27 -0300

    feat:cards updated

--- src/utils/sessionStorage.js
+++ src/utils/sessionStorage.js
@@ -1,13 +1,23 @@
+// Verifica se o código está sendo executado no lado do cliente (navegador)
 const isClient = typeof window !== "undefined";
 
+// Função para obter dados do sessionStorage
 export const getSessionStorage = (key, initialValue) => {
-    if (!isClient) return initialValue;
-    const stored = sessionStorage.getItem(key);
-    return stored ? JSON.parse(stored) : initialValue;
+  // Se não for cliente (executando no servidor), retorna o valor inicial fornecido
+  if (!isClient) return initialValue;
+
+  // Tenta pegar o item do sessionStorage com a chave fornecida
+  const stored = sessionStorage.getItem(key);
+
+  // Se o item estiver armazenado, retorna o valor parseado, caso contrário, retorna o valor inicial
+  return stored ? JSON.parse(stored) : initialValue;
 };
 
+// Função para armazenar dados no sessionStorage
 export const setSessionStorage = (key, value) => {
-    if (isClient) {
-        sessionStorage.setItem(key, JSON.stringify(value));
-    }
+  // Se for cliente (executando no navegador)
+  if (isClient) {
+    // Armazena o valor fornecido no sessionStorage, convertendo para string
+    sessionStorage.setItem(key, JSON.stringify(value));
+  }
 };
